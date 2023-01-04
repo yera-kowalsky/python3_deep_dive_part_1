764239f360305c0d9e87b84e9b164033cdba6069
Yerkebulan <yerkebulan014@gmail.com>
2023-01-04 16:45:47 +0100

    updated

--- 1_basics_review.py
+++ 1_basics_review.py
@@ -1,11 +1,70 @@
-a = [1  # item1
-    , 2]
-print(a)
+# a = [1  # item1
+#     , 2]
+# print(a)
+#
+# b = {'key1': 1 #comment1
+# ,'key2': 2 # comment2
+# }
+# print(b)
+# a = 10
+# b = 20
+# c = 30
+# if a > 5 \
+#     and b > 10 \
+#         and c > 20:
+#     print('yes')
+#
+# lambda x: x**2
+# func_1 = lambda x: print(x**2)
+# func_1(2)
 
-b = {'key1': 1 #comment1
-,'key2': 2 # comment2
-}
-print(b)
-'''
-James bond
-'''
+# min_lenght = 2
+#
+# while True:
+#     name = input("Please enter your name: ")
+#     if len(name) >= min_lenght and name.isprintable() and name.isalpha():
+#         break
+#
+# print(f'Hello, {name}')
+#
+# a = 0
+#
+# while a < 10:
+#     a += 1
+#     if a % 2 == 0:
+#         continue # ignores
+#     print(a)
+#
+# l = [1,2,3]
+# val = 10
+#
+# found = False
+# index = 0
+#
+# while index < len(l):
+#     if l[index] == val:
+#         found = True
+#         break
+#     index += 1
+#
+# if not found:
+#     l.append(val)
+# print(l)
+
+a = 10
+b = 0
+
+try:
+    a / b
+except ZeroDivisionError:
+    print('division by 0')
+finally:
+    print('this always executes')
+
+
+
+
+
+
+
+
