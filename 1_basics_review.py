2e7401e69f5c468b2d9e0d891ddfb7b579a53859
Yerkebulan <yerkebulan014@gmail.com>
2023-01-04 15:22:49 +0100

    Update 1_basics_review.py

--- 1_basics_review.py
+++ 1_basics_review.py
@@ -1,6 +1,11 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
+a = [1  # item1
+    , 2]
+print(a)
+
+b = {'key1': 1 #comment1
+,'key2': 2 # comment2
+}
+print(b)
+'''
+James bond
+'''
